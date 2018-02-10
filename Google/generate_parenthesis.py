c084796bbc8989f990172d46ee86aba50805d1a1
ssriram1978 <ssriram.78@gmail.com>
2018-02-10 16:35:07 -0500

    Google coding interview questions.

--- Google/generate_parenthesis.py
+++ Google/generate_parenthesis.py
@@ -15,8 +15,43 @@ For example, given n = 3, a solution set is:
 
 
 class Solution:
+
+    def __init__(self):
+        self.output_list=[]
+        self.count=0
+
+    def get_open_parenthesis(self):
+        return '('
+
+    def get_closed_parenthesis(self):
+        return ')'
+
+    def recursive_output(self,temp_string="",left_parenthesis=0,right_parenthesis=0):
+
+        if len(temp_string)== 2*self.count:
+            #base case
+            #you reached the max length, copy it to the output list
+            self.output_list.append(temp_string)
+            return
+
+        #if the left parenthesis count is less than the specified count, then, add it to temp string
+        if left_parenthesis < self.count:
+            temp_string+=self.get_open_parenthesis()
+            self.recursive_output(temp_string,left_parenthesis+1,right_parenthesis)
+
+        #finish off pairing left parenthesis with right parenthesis
+        if right_parenthesis < left_parenthesis:
+            temp_string+=self.get_closed_parenthesis()
+            self.recursive_output(temp_string,left_parenthesis,right_parenthesis+1)
+
     def generateParenthesis(self, n):
         """
         :type n: int
         :rtype: List[str]
         """
+        self.count=n
+        self.recursive_output()
+        return self.output_list
+
+sol=Solution()
+print(sol.generateParenthesis(10))

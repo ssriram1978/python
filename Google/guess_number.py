184077f4ba5ebeb3812578c74b4a191288e4e917
ssriram1978 <ssriram.78@gmail.com>
2018-02-10 17:06:51 -0500

    Google coding interview questions.

--- Google/guess_number.py
+++ Google/guess_number.py
@@ -21,8 +21,39 @@ Return 6.
 # def guess(num):
 
 class Solution(object):
+    def __init__(self):
+        self.actual_number=1034673429707
+    def guess(self,number):
+        if number == self.actual_number:
+            return 0
+        elif number > self.actual_number:
+            return -1
+        else:
+            return 1
+
     def guessNumber(self, n):
         """
         :type n: int
         :rtype: int
         """
+        if n <1:
+            return 0
+        low=1
+        high=n
+        iterations=0
+        mid=0
+        while low <= high:
+            iterations+=1
+            mid=low+(high-low)//2
+            result=self.guess(mid)
+            print("iterations=%d,low=%d,high=%d" % (iterations, low, high))
+            if result == 0:
+                break
+            elif result == 1:
+                low=mid+1
+            elif result == -1:
+                high=mid-1
+        return mid
+
+sol=Solution()
+print(sol.guessNumber(100000000000000))

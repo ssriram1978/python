f38c506e22fc5ad676a23e454545425c54ff91e8
Sriram Sridhar <sriram.sridhar@verizon.com>
2018-02-20 13:31:05 -0500

    Coding Interview questions.

--- Coding Interview Questions/sort_colors.py
+++ Coding Interview Questions/sort_colors.py
@@ -16,11 +16,60 @@ then 1's and followed by 2's.
 
 Could you come up with an one-pass algorithm using only constant space?
 """
+"""
+Algorithm:
+----------
+The problem was posed with three colours, here `0′, `1′ and `2′. The array is divided into four sections:
 
+a[1..Lo-1] zeroes (red)
+a[Lo..Mid-] ones (white)
+a[Mid..Hi] unknown
+a[Hi+1..N] twos (blue)
+The unknown region is shrunk while maintaining these conditions
+
+Lo := 1; Mid := 1; Hi := N;
+while Mid <= Hi do
+Invariant: a[1..Lo-1]=0 and a[Lo..Mid-1]=1 and a[Hi+1..N]=2; a[Mid..Hi] are unknown.
+case a[Mid] in
+0: swap a[Lo] and a[Mid]; Lo++; Mid++
+1: Mid++
+2: swap a[Mid] and a[Hi]; Hi–
+— Dutch National Flag Algorithm, or 3-way Partitioning —
+"""
 
 class Solution(object):
+    def swap(self,array,index1,index2):
+        temp=array[index1]
+        array[index1]=array[index2]
+        array[index2]=temp
+
     def sortColors(self, nums):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        if nums == None or len(nums) == 0:
+            return
+
+        low=0
+        mid=0
+        high=len(nums)-1
+
+        while mid < high:
+            if nums[mid] == 0:
+                #swap it with low
+                self.swap(nums,mid,low)
+                low+=1
+                mid+=1
+            elif nums[mid] == 1:
+                #let it remain in the same place
+                mid+=1
+            elif nums[mid] == 2:
+                #swap high with mid
+                self.swap(nums,high,mid)
+                high-=1
+
+sol=Solution()
+nums=[0,2,1,1,2,2,2,2,2,2]
+sol.sortColors(nums)
+print(nums)
--- Coding Interview Questions/strstr.py
+++ Coding Interview Questions/strstr.py
@@ -14,6 +14,20 @@ Output: -1
 
 """
 
+"""
+Algorithm:
+----------
+Declare an output temp variable to be -1 which will be used to store the index of the first match.
+Search for the first occurance of the first character of needle in haystack.
+If found, then, do the following:
+    Store the index of the first character match as the output temp variable.
+    Check if the characters preceding needle match the characters preceding haystack.
+        If they match,store and return the output temp variable.
+    Else
+        Reset the output temp variable to -1
+Return the output temp variable
+    
+"""
 
 class Solution(object):
     def strStr(self, haystack, needle):
@@ -22,3 +36,37 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
+        if needle == None or needle == "" or haystack == None or haystack == "":
+            return -1
+
+        #declare first found index to be -1
+        firstFoundIndex=-1
+
+        for current_index in range(len(haystack)):
+            #check if the first character in needle matches with the character in haystack at current_index.
+            if needle[0] == haystack[current_index]:
+                #declare a temp variable that stores this current index
+                firstPossibleMatch=current_index
+                #check if the rest of the characters preceding needle match
+                # with the characters preceding haystack at current index.
+                needle_index=1
+                haystack_index=current_index+1
+                while needle_index < len(needle):
+                    if needle[needle_index] != haystack[haystack_index]:
+                        #No match found,break from the while loop
+                        break
+                    needle_index+=1
+                    haystack_index+=1
+
+                if needle_index == len(needle):
+                    #complete match found
+                    firstFoundIndex=firstPossibleMatch
+                    break
+
+        return firstFoundIndex
+
+sol=Solution()
+index=sol.strStr("hello world","world")
+print(index)
+index=sol.strStr("wonworwwwowobwwwolwwwwol","wol")
+print(index)

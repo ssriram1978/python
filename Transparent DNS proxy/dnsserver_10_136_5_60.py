2c2040601fc9f0dfd2694c4a0de6b1d618e8ca6e
Sriram Sridhar <sriram.sridhar@verizon.com>
2018-02-27 10:47:16 -0500

    DNS PROXY

--- Transparent DNS proxy/dnsserver_10_136_5_60.py
+++ Transparent DNS proxy/dnsserver_10_136_5_60.py
@@ -21,80 +21,6 @@ IP_BINDANY=24
 MAC_OF_BBI_NEXT_HOP="e4:d3:f1:fc:75:00"
 MAC_OF_DNS_CONT="60:00:00:00:00:00"
 
-def match_dns_query_name_with_whitelist_blacklist(data):
-    request = DNSRecord.parse(data)
-    print("request=%s" %(str(request)))
-    qname = str(request.q.qname).strip('.')
-    print("qname=" + qname)
-    #qtype = request.q.qtype
-
-    if qname in open(whitelist_blacklist_file_name).read():
-        success = 0
-        q = DNSRecord.question(qname)
-        reply = q.reply()
-        if success == 1 :
-            reply = DNSRecord(DNSHeader(id=request.header.id, opcode=2, rcode=5, qr=1, aa=1, ra=0), q=request.q)
-            denied_string="VISP: Sorry, this website is restricted by your admin."
-            #reply.add_answer(RR(rname=qname, rtype=2, rclass=1, ttl=TTL, rdata=denied_string))
-            reply.add_answer(RR(qname, QTYPE.SOA, rdata=SOA(denied_string)))
-        else:
-            reply.add_answer(RR(qname, QTYPE.A, rdata=A("1.2.3.4"), ttl=60))
-        reply.header.id = request.header.id
-        #print("request.header.id=%d\n" %(request.header.id))
-        #print("reply.header.id=%d\n" %(reply.header.id))
-        print("---- Reply:%s\n" %(reply))
-        return reply.pack()
-    else :
-        return None
-
-def match_dns_query_name_with_whitelist_blacklist2(data):
-    request = DNSRecord.parse(data)
-    print("request=%s" %(str(request)))
-    qname = str(request.q.qname).strip('.')
-    print("qname=" + qname)
-    #qtype = request.q.qtype
-
-    if qname in open(whitelist_blacklist_file_name).read():
-        success = 0
-        q = DNSRecord.question(qname)
-        reply = q.reply()
-        if success == 1 :
-            reply = DNSRecord(DNSHeader(id=request.header.id, opcode=2, rcode=5, qr=1, aa=1, ra=0), q=request.q)
-            denied_string="VISP: Sorry, this website is restricted by your admin."
-            #reply.add_answer(RR(rname=qname, rtype=2, rclass=1, ttl=TTL, rdata=denied_string))
-            reply.add_answer(RR(qname, QTYPE.SOA, rdata=SOA(denied_string)))
-        else:
-            reply.add_answer(RR(qname, QTYPE.A, rdata=A("1.2.3.4"), ttl=60))
-        reply.header.id = request.header.id
-        #print("request.header.id=%d\n" %(request.header.id))
-        #print("reply.header.id=%d\n" %(reply.header.id))
-        print("---- Reply:%s\n" %(reply))
-        return reply.pack()
-    else :
-        return None
-
-def server():
-    s = socket.socket(AF_INET, SOCK_DGRAM,socket.IPPROTO_UDP)
-    s.setsockopt(socket.IPPROTO_IP, IP_TRANSPARENT, 1)
-    s.setsockopt(socket.IPPROTO_IP, IP_FREEBIND, 1)
-    s.setsockopt(socket.IPPROTO_IP, IP_BINDANY, 1)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    s.bind(('', PORT))
-    #fcntl.ioctl(s, SIO_RCVALL, 1)
-    #s.setsockopt(socket.SIO_RCVALL, socket.RCVALL_ON)
-    #s.setsockopt(socket.IPPROTO_IP,socket.SIO_RCVALL,1)
-
-    print('udp echo server ready')
-    while 1:
-        data, addr = s.recvfrom(BUFSIZE)
-        print('server received %r from %r' %(data, addr))
-        dns_response = match_dns_query_name_with_whitelist_blacklist(data)
-        if dns_response == None:
-            # just allow this DNS to pass through
-            print("dns response is none.\n")
-            s.send(data,addr)
-        else:
-            s.sendto(dns_response,addr)
 
 
 
@@ -130,8 +56,10 @@ def sniff_packet(pkt):
 
                 match_found=False
 
-                dns_reject=False
-                dns_redirect_to_static_webpage=True
+                #dns_reject=False
+                dns_reject=True
+                #dns_redirect_to_static_webpage=True
+                dns_redirect_to_static_webpage=False
 
                 for line in open(whitelist_blacklist_file_name) :
                     line=line.strip('\n')                    
@@ -151,8 +79,8 @@ def sniff_packet(pkt):
                             DNS_resp.an = DNSRR(rrname=qname, type=16, rclass=1, ttl=3600,
                                                 rdata='From VISP: Content restricted.')
                             logging.debug(
-                                "line=%s and qname=%s match.Composing and sending DNS Reject (%s).\n"
-                                %(line, qname, ip_addr))
+                                "line=%s and qname=%s match.Composing and sending DNS Reject response.\n"
+                                %(line, qname))
                         elif dns_redirect_to_static_webpage == True:
                             ip_addr="10.2.40.164"
                             DNS_resp.an=DNSRR(rrname=qname,type="A",rclass=1,ttl=120,rdata=ip_addr)
@@ -160,7 +88,7 @@ def sniff_packet(pkt):
                             "line=%s and qname=%s match.Composing and sending DNS response "
                             "pointing to a web server (%s).\n"
                             %(line, qname, ip_addr))
-
+                        
                         #response = DNSRecord.parse(bytes(DNS_resp))
                         dns_response_packet = Ether(src=pkt[Ether].dst,dst=pkt[Ether].src) /\
                                           IP(src=str(ip_dst), dst=str(ip_src)) / \

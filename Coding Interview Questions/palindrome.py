2cd1a91a53922eeef72ed98ffcb35bd89c7b7f9a
Sriram Sridhar <ssriram.78@gmail.com>
2018-02-27 16:59:19 -0500

    Update palindrome.py

--- Coding Interview Questions/palindrome.py
+++ Coding Interview Questions/palindrome.py
@@ -1,3 +1,10 @@
+"""
+To check if an integer is a palindrome:
+In a loop until you have extracted all the digits from the end of the number,
+    Extract each last digit of the integer via % 10 and keep adding it to result * 10.
+If the computed reversed number is equal to the original number, then return True else return False.
+"""
+
 class Solution:
     def isPalindrome(self, x):
         """
@@ -7,17 +14,18 @@ class Solution:
         if x <= 0 or (x > 0 and x < 10):
             return False
 
-        sum = 0
-        temp = int(x)
-        while int(temp) != 0:
-            sum = int(sum) * 10 + (int(temp)%10)
-            print("temp=%d x=%d and sum=%d\n" % (temp, x,sum))
-            temp = int(temp) / 10
+        reversed_number = 0
+        temp = x
+        while temp != 0:
+            reminder= temp%10
+            reversed_number=reversed_number*10+ reminder
+            #get rid of the last digit
+            temp = temp//10
 
-        if int(sum) == int(x):
+        if reversed_number == x:
             return True
         else:
             return False
 
 sol=Solution()
-print(sol.isPalindrome(121))
+print(sol.isPalindrome(112211))

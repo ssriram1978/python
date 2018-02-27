b50c6adac97a5f1addc5587e5ece96ce2abff21d
Sriram Sridhar <sriram.sridhar@verizon.com>
2018-02-27 10:48:01 -0500

    Coding Interview questions.

--- Coding Interview Questions/shortest_palindrome.py
+++ Coding Interview Questions/shortest_palindrome.py
@@ -17,8 +17,101 @@ string to make it a palindrome.
 """
 
 class Solution(object):
+
+    def checkIsPalindrome(self,s):
+        isPalindrome=False
+        if s == None or len(s) == 0 or len(s) == 1:
+            return isPalindrome
+
+        mid = len(s)//2
+        left = 0
+        right = 0
+
+        if len(s) % 2 == 0:
+            # it is even
+            # check the two mid characters to be equal
+            mid1 = len(s) // 2
+            mid2 = mid - 1
+            if s[mid1] != s[mid2]:
+                return isPalindrome
+            left=mid1-1
+            right=mid2+1
+        else:
+            # it is odd
+            mid = len(s) // 2
+            left = mid - 1
+            right = mid + 1
+
+        while left >=0 and right <=len(s)-1:
+            if s[left]!=s[right]:
+                break
+            left-=1
+            right+=1
+
+        if left == -1 and right==len(s):
+            isPalindrome=True
+
+        return isPalindrome
+
     def shortestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
+        isPalindrome = True
+        output_string=""
+
+        if s == None or len(s) == 0:
+            isPalindrome=False
+            return None
+
+        if len(s) ==1:
+            return s+s
+
+        mid = len(s) // 2
+        left = 0
+        right = 0
+
+        if len(s) % 2 == 0:
+            # it is even
+            # check the two mid characters to be equal
+            mid1 = len(s) // 2
+            mid2 = mid - 1
+            if s[mid1] != s[mid2]:
+                isPalindrome=False
+            left = mid1 - 1
+            right = mid2 + 1
+        else:
+            # it is odd
+            mid = len(s) // 2
+            left = mid - 1
+            right = mid + 1
+
+        if isPalindrome == False:
+            output_string+=s[::-1]+s[1::]
+            return output_string
+
+        while left >= 0 and right <= len(s) - 1:
+            if s[left] != s[right]:
+                break
+            left -= 1
+            right += 1
+
+        if left == -1 and right == len(s):
+            #perfect palindrome
+            output_string=s
+        elif left == -1 and right < len(s):
+            #you still have some characters left on the left side of mid
+            #but your right index reached the beginning
+            #append the rest of the characters to the output string and return it as the palindrome
+            output_string+=s[-1:right]+s
+        elif left != -1 and right == len(s):
+            #you still have some characters left on the right side of mid
+            #but your left index reached the end
+            #prepend the whole reversed string to the output string and return it as the palindrome
+            output_string+=s[::-1]+s[1::]
+
+        return output_string
+
+sol=Solution()
+print(sol.shortestPalindrome("abaa"))

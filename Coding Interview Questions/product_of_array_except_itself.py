801fa8dc94f47c5129a24b4582c8343877e054c2
Sriram Sridhar <ssriram.78@gmail.com>
2018-02-27 18:15:48 -0500

    Update product_of_array_except_itself.py

--- Coding Interview Questions/product_of_array_except_itself.py
+++ Coding Interview Questions/product_of_array_except_itself.py
@@ -12,7 +12,12 @@ Could you solve it with constant space complexity?
 (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
 
 """
-
+"""
+Algorithm:
+1) Construct a temporary array left[] such that left[i] contains product of all elements on left of arr[i] excluding arr[i].
+2) Construct another temporary array right[] such that right[i] contains product of all elements on on right of arr[i] excluding arr[i].
+3) To get prod[], multiply left[] and right[].
+"""
 
 class Solution(object):
     def productExceptSelf(self, nums):

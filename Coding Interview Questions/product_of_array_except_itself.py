2f76d595f2cecd982d8c56aff6465f0b7a6e8497
Sriram Sridhar <ssriram.78@gmail.com>
2018-02-28 10:17:26 -0500

    Update product_of_array_except_itself.py

--- Coding Interview Questions/product_of_array_except_itself.py
+++ Coding Interview Questions/product_of_array_except_itself.py
@@ -12,16 +12,58 @@ Could you solve it with constant space complexity?
 (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
 
 """
+
 """
-Algorithm:
-1) Construct a temporary array left[] such that left[i] contains product of all elements on left of arr[i] excluding arr[i].
-2) Construct another temporary array right[] such that right[i] contains product of all elements on on right of arr[i] excluding arr[i].
-3) To get prod[], multiply left[] and right[].
+Define a left array which multiplies all the elements before the current element.
+Define a right array which multiplies all the elements after the current element.
+Multiply left array and right array which results in the desired output.
 """
 
 class Solution(object):
+    def prepare_left_array(self,nums):
+        left_array=[0]*len(nums)
+        product=0
+        for index in range(len(nums)):
+            element=nums[index]
+            if index == 0:
+                #skip this element.
+                product=element
+                left_array[index]=1
+            else:
+                left_array[index]=product
+                product*=element
+        return left_array
+
+    def prepare_right_array(self,nums):
+        right_array=[0]*len(nums)
+        product=0
+        for index in range(len(nums)-1,-1,-1):
+            element=nums[index]
+            if index == len(nums)-1:
+                #skip this element
+                product=element
+                right_array[index]=1
+            else:
+                right_array[index]=product
+                product*=element
+        return right_array
+
+    def prepare_output(self,left_array,right_array):
+        output_array=[0]*len(left_array)
+        for index in range(len(left_array)):
+            output_array[index]=left_array[index]*right_array[index]
+        return output_array
+
     def productExceptSelf(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
+        left_array=self.prepare_left_array(nums)
+        right_array=self.prepare_right_array(nums)
+        output=self.prepare_output(left_array,right_array)
+        return output
+
+sol=Solution()
+input=[1,2,3,4]
+print(sol.productExceptSelf(input))

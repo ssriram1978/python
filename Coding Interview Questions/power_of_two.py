115f61b36ceb5aa448df33003b1ff3b9a13b298a
Sriram Sridhar <ssriram.78@gmail.com>
2018-02-28 17:25:09 -0500

    Update power_of_two.py

--- Coding Interview Questions/power_of_two.py
+++ Coding Interview Questions/power_of_two.py
@@ -2,11 +2,13 @@
 Given an integer, write a function to determine if it is a power of two.
 """
 """
-check if the number is fully divisible by 2
-get the quotient
-use this quotient and check if is fully divisible by 2
-now take the quotient and the original number
-multiply them and check if the result is fully divisible by 2
+2=10
+4=100
+8=1000
+16=10000
+...
+If it is a 64 bit number, keep extracting the LSB 64 times and make sure that you have just one 1 and 63 zeros.
+
 """
 
 class Solution:
@@ -15,3 +17,25 @@ class Solution:
         :type n: int
         :rtype: bool
         """
+        isPowOfTwo=False
+        mask=1
+        found_one=False
+        for index in range(0,64):
+            if n&mask==1:
+                if found_one==True:
+                    isPowOfTwo=False
+                    break
+                else:
+                    found_one=True
+            #remember to left shift the number by 1
+            n=n>>1
+
+        if found_one==True and index==63:
+            isPowOfTwo=True
+        return isPowOfTwo
+
+sol=Solution()
+print(sol.isPowerOfTwo(1024))
+print(sol.isPowerOfTwo(4096))
+print(sol.isPowerOfTwo(100))
+print(sol.isPowerOfTwo(1125899906842624))

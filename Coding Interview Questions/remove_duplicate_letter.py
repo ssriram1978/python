a570a3fbc97df9fc00f14d2a146020daf0c52bbe
Sriram Sridhar <ssriram.78@gmail.com>
2018-02-28 16:24:33 -0500

    Update remove_duplicate_letter.py

--- Coding Interview Questions/remove_duplicate_letter.py
+++ Coding Interview Questions/remove_duplicate_letter.py
@@ -23,10 +23,60 @@ The smallest letter from index 4 to index 4: d
 The smallest letter from index 5 to index 6: b
 So the result is "acdb"
 """
-
+from collections import defaultdict
 class Solution:
+    def getSortedList(self,s):
+        string_list = []
+        for index in range(len(s)):
+            character = s[index]
+            string_list.append((character, index))
+        string_char_sorted_list = sorted(string_list, key=lambda x: x[0])
+        return string_char_sorted_list
+
+    def getMinIndexForMinChar(self,s,string_list):
+        # start the output from the first character in the sorted list
+        value = string_list[0]
+        first_character = value[0]
+        # search for the first character in the original string
+        starting_index = s.find(first_character)
+        return starting_index
+
+    def PrepareMinString(self,input_string,sorted_list,starting_index):
+        hash_table_char=defaultdict(int)
+        output_string=""
+
+        for index in range(starting_index,len(input_string)):
+            if hash_table_char[input_string[index]]==0:
+                #add it to hash table and output string
+                hash_table_char[input_string[index]]=1
+                output_string+=input_string[index]
+
+        #make sure that you did not miss out any unique non repeating characters before the starting index.
+        for index in range(0,starting_index):
+            if hash_table_char[input_string[index]]==0:
+                #you found a unique character
+                #prepend it to the output string
+                output_string=input_string[index]+output_string
+                hash_table_char[input_string[index]] = 1
+                
+        return output_string
+
     def removeDuplicateLetters(self, s):
         """
         :type s: str
         :rtype: str
         """
+        if s== None or s == "" or type(s)!=str:
+            return None
+        #get the sorted char list with index.
+        string_list=self.getSortedList(s)
+        #get the first char and the min index of the first char in s.
+        starting_index=self.getMinIndexForMinChar(s,string_list)
+        #prepare the string in lexicographic order and return it.
+        output=self.PrepareMinString(s,string_list,starting_index)
+
+        return output
+
+s=Solution()
+
+print(s.removeDuplicateLetters("eeeeeeccccccccdddddddda"))

15ec8e90ba16b10acd753e3e20ccbdadb115a1d9
Sriram Sridhar <ssriram.78@gmail.com>
2018-02-28 10:53:39 -0500

    Update remove_duplicates_from_sorted.py

--- Coding Interview Questions/remove_duplicates_from_sorted.py
+++ Coding Interview Questions/remove_duplicates_from_sorted.py
@@ -11,10 +11,34 @@ Your function should return length = 2, with the first two elements of nums bein
 It doesn't matter what you leave beyond the new length.
 """
 
+"""
+Given that you have a sorted array,
+All you need to do is remove the duplicates.
+Start a while loop until you reach the end of the array:
+    Check if the content of current index is equal to the content of current index +1
+        If they match, then, delete the current content.
+        Else
+        Skip the current content move to the next element in the array.
+return the output.
+"""
 
-class Solution(object):
+class Solution:
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        if nums == None or len(nums) == 0:
+            print("Invalid arguments")
+            return None
+        current_index=0
+        while current_index < len(nums)-1:
+            if nums[current_index] == nums[current_index+1]:
+                del nums[current_index]
+            else:
+                current_index+=1
+
+sol=Solution()
+list=[1,1,1,1,2,3,3]
+sol.removeDuplicates(list)
+print(list)

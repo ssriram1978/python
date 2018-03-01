97a6799fc868c760be03652dda289f29e6236459
Sriram Sridhar <ssriram.78@gmail.com>
2018-03-01 14:26:19 -0500

    Update count_smaller_number_after_self.py

--- Coding Interview Questions/count_smaller_number_after_self.py
+++ Coding Interview Questions/count_smaller_number_after_self.py
@@ -16,7 +16,18 @@ Return the array [2, 1, 1, 0].
 """
 """
 1. Brute force: Use two for loops. o(n**2)
+Use two loops. The outer loop picks all elements from left to right. 
+The inner loop iterates through all the elements on right side of the picked element and updates countSmaller[].
+
 2. AVL tree: o(nlogn)
+A Self Balancing Binary Search Tree (AVL, Red Black,.. etc) can be used to get the solution in O(nLogn) time complexity.
+We can augment these trees so that every node N contains size the subtree rooted with N.
+We have used AVL tree in the following implementation.
+We traverse the array from right to left and insert all elements one by one in an AVL tree.
+While inserting a new key in an AVL tree, we first compare the key with root.
+If key is greater than root, then it is greater than all the nodes in left subtree of root.
+So we add the size of left subtree to the count of smaller element for the key being inserted.
+We recursively follow the same approach for all nodes down the root.
 """
 
 class Solution(object):

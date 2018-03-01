63f61151edab263a40b88c992120bf529c8928c9
Sriram Sridhar <ssriram.78@gmail.com>
2018-03-01 14:13:15 -0500

    Update count_and_say_seq.py

--- Coding Interview Questions/count_and_say_seq.py
+++ Coding Interview Questions/count_and_say_seq.py
@@ -22,7 +22,29 @@ Example 2:
 Input: 4
 Output: "1211"
 """
+"""
+Algorithm:
+----------
+Given nth sequence, you need to compute all the sequence from 0 to n-1 and then
+compute nth sequence.
+For this:
+declare output_list=[] to capture all the sequences.
+if n==1 return "1"
+if n==2 return "11"
+else
+    Append "1","11" to output_list.
+    Declare previous_element="11"
+    Now, start a while loop until the count reaches n-1
+        Declare next_element=""
+        Read the current character from the previous element:
+        count=1
+        Start a while loop to count the consecutive occurrence of this character.
+        Prepend this count in the next_element.
+        Append the character to the next_element.
 
+    Append next element to the output_list
+    return output_list[n-1]
+"""
 
 class Solution(object):
     def countAndSay(self, n):

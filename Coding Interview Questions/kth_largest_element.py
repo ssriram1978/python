c84a845889b66207086f533b0ab965bc465c005b
Sriram Sridhar <ssriram.78@gmail.com>
2018-03-02 11:01:04 -0500

    Update kth_largest_element.py

--- Coding Interview Questions/kth_largest_element.py
+++ Coding Interview Questions/kth_largest_element.py
@@ -7,7 +7,11 @@ Given [3,2,1,5,6,4] and k = 2, return 5.
 Note:
 You may assume k is always valid, 1 ≤ k ≤ array's length.
 """
-
+"""
+1. Sort the numbers in the order of O(nlogn) HEAP sort, Quick(pivot) sort - in place O(1)space complexity or
+    Use mergesort O(n) space complexity.
+2. Directly index into the sorted array at the given index and return the value. O(1)
+"""
 
 class Solution(object):
     def findKthLargest(self, nums, k):

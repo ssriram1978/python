95b61da9b86a1af670074155d392709359eb4f0b
Sriram Sridhar <ssriram.78@gmail.com>
2018-03-02 11:48:44 -0500

    Update lowest_common_ancester_bst.py

--- Coding Interview Questions/lowest_common_ancester_bst.py
+++ Coding Interview Questions/lowest_common_ancester_bst.py
@@ -17,6 +17,22 @@ Another example is LCA of nodes 2 and 4 is 2,
 since a node can be a descendant of itself according to the LCA definition.
 
 """
+"""
+Algorithm:
+----------
+If we are given a BST where every node has parent pointer, 
+then LCA can be easily determined by traversing up using parent pointer and printing the first intersecting node.
+We can solve this problem using BST properties. 
+We can recursively traverse the BST from root. 
+The main idea of the solution is, while traversing from top to bottom, 
+                                 the first node n we encounter with value between n1 and n2, 
+                                 i.e., n1 < n < n2 or same as one of the n1 or n2, is LCA of n1 and n2 
+                                 (assuming that n1 < n2). 
+So just recursively traverse the BST in, 
+    if node’s value is greater than both n1 and n2 then our LCA lies in left side of the node, 
+    if it’s is smaller than both n1 and n2, then LCA lies on right side. 
+    Otherwise root is LCA (assuming that both n1 and n2 are present in BST)
+"""
 
 
 # Definition for a binary tree node.

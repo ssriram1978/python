ba381d3959006271501bf858db8df879cde791bb
Sriram Sridhar <ssriram.78@gmail.com>
2018-03-02 09:00:02 -0500

    Update group_anagrams.py

--- Coding Interview Questions/group_anagrams.py
+++ Coding Interview Questions/group_anagrams.py
@@ -11,7 +11,14 @@ Return:
 ]
 Note: All inputs will be in lower-case.
 """
-
+"""
+Start a loop that walks over each word in the list O(n) where n is the number of words in the list.
+    Sort each word in the order of XlogX where X is the number of characters in each word.
+    Add this sorted word into a dictionary with
+        Key=sorted word
+        Value=list of actual words.
+Now walk through the dict to fetch the key,value pairs and append only the values to a output list and return it. 
+"""
 
 class Solution(object):
     def groupAnagrams(self, strs):

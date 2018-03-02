cba3e04df6f7dc87f8f73cb0bd30b6661a34891f
Sriram Sridhar <ssriram.78@gmail.com>
2018-03-02 09:10:03 -0500

    Update hamming_distance.py

--- Coding Interview Questions/hamming_distance.py
+++ Coding Interview Questions/hamming_distance.py
@@ -19,6 +19,27 @@ Explanation:
 
 The above arrows point to positions where the corresponding bits are different.
 """
+"""
+Declare hamming_distance variable as 0.
+Start a while loop which goes while x is not equal to 0 and y is not equal to 0.
+    And x with 1 to check if LSB is set. If so, set a variable x_1 as True else variable x_1=False
+    And y with 1 to check if LSB is set. If so, set a variable y_1 as True else variable y_1=False
+    If x_1 == True and y_1==False or x_1==False and y_1==True:
+        hamming_distance+=1
+    Right shift x and y by 1.
+If x is not equal to 0:
+    You still have some left overs on x:
+    Start a while loop until x is not equal to 0:
+        And x with 1 and check if LSB is set. If so, increment hamming_distance by 1 because 
+        you already know that y is 0.
+Else:
+If y is not equal to 0:
+    You still have some left overs on y:
+    Start a while loop until y is not equal to 0:
+        And y with 1 and check if LSB is set. If so, increment hamming_distance by 1 because 
+        you already know that x is 0.
+return hamming_distance.
+"""
 
 
 class Solution(object):

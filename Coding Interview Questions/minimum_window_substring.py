ed736cdd42a9c4832630f15570074e3a98df3fca
Sriram Sridhar <ssriram.78@gmail.com>
2018-03-02 13:36:40 -0500

    Update minimum_window_substring.py

--- Coding Interview Questions/minimum_window_substring.py
+++ Coding Interview Questions/minimum_window_substring.py
@@ -12,10 +12,15 @@ If there is no such window in S that covers all characters in T, return the empt
 
 If there are multiple such windows,
 you are guaranteed that there will always be only one unique minimum window in S.
-
-
 """
-
+"""
+For every character in S, add it to a dictionary with the key as the character and the index as the value. O(n)
+For every character in T: O(m)
+    Look up the character in the dictionary and get the list. O(1)
+    Keep appending this list to an output_list.
+Now, you have the output list which contains the list of each character index.
+You need to find the minimal distance between the indexes.
+"""
 
 class Solution(object):
     def minWindow(self, s, t):

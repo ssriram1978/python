e70f37c1d3453678bce1fb60e1b58abbf28470aa
ssriram1978 <ssriram.78@gmail.com>
2018-03-03 19:55:03 -0500

    Reorg

---
+++ Algorithms/majority_element.py
@@ -0,0 +1,35 @@
+"""
+Majority Element is an element that makes up more than half of the items in the array.
+Given positive integers array, find the majority element.
+If there is no majority element, return -1.
+Do this O(N) time and O(1) space.
+Input: 1 2 5 9 5 9 5 5 5
+Output: 5
+"""
+
+class Solution:
+    def getCandidate(self,array):
+        majority=0
+        count=0
+        for n in array:
+            if count==0:
+                print("Setting %d as majority"%(n))
+                majority=n
+            if n == majority:
+                count+=1
+            else:
+                count-=1
+        return majority
+    def validate(self,array,majority):
+        count=0
+        for n in array:
+            if n == majority:
+                count+=1
+        return count
+    def findMajorityElement(self,array):
+        candidate=self.getCandidate(array)
+        return self.validate(array,candidate)
+
+sol=Solution()
+array=[1,2,5,9,10,5,9,5,5,5]
+print(sol.findMajorityElement(array))

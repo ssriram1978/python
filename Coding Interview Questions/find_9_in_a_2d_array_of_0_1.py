80aa870867af5018da89d9da79fd7d51a5457c97
Sriram Sridhar <>
2018-10-30 10:16:36 -0400

    Computed minimum distance to the destination when there are multiple paths to the same destination.

--- Coding Interview Questions/find_9_in_a_2d_array_of_0_1.py
+++ Coding Interview Questions/find_9_in_a_2d_array_of_0_1.py
@@ -13,6 +13,34 @@ input_puzzle = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                 [0, 0, 1, 1, 0, 0, 1, 0, 0, 1],
                 [0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
                 [0, 0, 0, 1, 1, 9, 1, 1, 1, 1]]
+"""
+Amazon coding interview test.
+-----------------------------
+Starting from the left most corner of the puzzle, 
+traverse up, down, left or right in such a way that you can step on only 1's and not on 0's.
+Count the number of 1's that lead to the final destination 9.
+
+Maintain two reference copies.
+1. Which stores if the node is already traversed or not. (already_visited_nodes) to avoid infinite loop.
+2. Which stores the minimum distance to reach the destination from each node. (Memoization).
+
+Recursive algorithm:
+---------------------
+conditions:
+1. Check for boundary conditions and back off from the recursive loop.
+2. If the current node is 9, then, append 1 to the number_of_1s list and return True.
+3. If the current node is 0, then return False.
+4. If already_visited_nodes is set to 1 for the current row, column, then return False. (avoid recursive loop)
+5. If the current node is 1, then,
+   a. Recurse Left. Count the number of 1s if you found 9.
+   b. Recurse Right. Count the number of 1s if you found 9.
+   c. Recurse Down. Count the number of 1s if you found 9.
+   d. Find the min of previous 3 steps (a,b,c) and store it in the min_distance_computed_nodes. (memoization)
+   e. Use this computed min_distance_computed_nodes in step a,b,c if found and avoid recursive loops. (2**n)
+   f. Based upon the min found at step d, prepare an output list with 1's appended to it.
+6. The calling function should check for return value of this function and if the return value is True,
+   the calling function should count the number of 1s in the num_of_1s list and use this as the final answer.
+"""
 
 def compute_min_length(row,
                        column,
@@ -39,32 +67,6 @@ def compute_min_length(row,
                 min_computed_length = len(temp_number_of_1s)
     return min_computed_length
 
-"""
-Starting from the left most corner of the puzzle, 
-traverse up, down, left or right in such a way that you can step on only 1's and not on 0's.
-Count the number of 1's that lead to the final destination 9.
-
-Maintain two reference copies.
-1. Which stores if the node is already traversed or not. (already_visited_nodes) to avoid infinite loop.
-2. Which stores the minimum distance to reach the destination from each node. (Memoization).
-
-Recursive algorithm:
----------------------
-conditions:
-1. Check for boundary conditions and back off from the recursive loop.
-2. If the current node is 9, then, append 1 to the number_of_1s list and return True.
-3. If the current node is 0, then return False.
-4. If already_visited_nodes is set to 1 for the current row, column, then return False. (avoid recursive loop)
-5. If the current node is 1, then,
-   a. Recurse Left. Count the number of 1s if you found 9.
-   b. Recurse Right. Count the number of 1s if you found 9.
-   c. Recurse Down. Count the number of 1s if you found 9.
-   d. Find the min of previous 3 steps (a,b,c) and store it in the min_distance_computed_nodes. (memoization)
-   e. Use this computed min_distance_computed_nodes in step a,b,c if found and avoid recursive loops. (2**n)
-   f. Based upon the min found at step d, prepare an output list with 1's appended to it.
-6. The calling function should check for return value of this function and if the return value is True,
-   the calling function should count the number of 1s in the num_of_1s list and use this as the final answer.
-"""
 def destination_found(row,
                       column,
                       number_of_1s,

27656a6d2e96473353b2241181f54749a7139fc2
ssriram78 <CodingSkill13!>
2018-12-17 02:00:39 +0000

    More dynamic programming.

--- dynamic_programming/climbing_stairs.py
+++ dynamic_programming/climbing_stairs.py
@@ -21,32 +21,109 @@ Explanation: There are three ways to climb to the top.
 2. 1 step + 2 steps
 3. 2 steps + 1 step
 
+If n is the total number of stairs and
+m is the max number of steps to climb at one time.
+ClimbStairs(n) = ClimbStairs(n-1) + ClimbStairs(n-2) + ClimbStairs(n-m)
+
 """
+
+
 class Solution:
 
-    def climbStairs_Memoization(self, n, cache):
-        if n == 1 or n == 2:
+    def recurse_n_steps_memo(self, n, m, cache):
+        if n == 0:
+            return 0
+        elif n == 1:
             return 1
+        elif n == 2:
+            return 2
         else:
-            if not cache[n-1]:
-                cache[n-1] = self.climbStairs_Memoization(n-1, cache)
-            if not cache[n-2]:
-                cache[n-2] = self.climbStairs_Memoization(n-2, cache)
-            return cache[n-1] + cache[n-2]
+            if not cache[n - 1]:
+                cache[n - 1] = self.recurse_n_steps_memo(n - 1,
+                                                         m,
+                                                         cache)
+            if not cache[n - 2]:
+                cache[n - 2] = self.recurse_n_steps_memo(n - 2,
+                                                         m,
+                                                         cache)
+            total_ways = 0
+            for index in range(1, m + 1):
+                total_ways += cache[n - index]
+            return total_ways
 
-    def climbStairs2(self, n):
+    def climbStairs_recursion_n_steps_memo(self, n, m):
         cache = [0] * n
-        return self.climbStairs_Memoization(n, cache)
+        return self.recurse_n_steps_memo(n, m, cache)
 
-    def climbStairs(self, n):
+    def climbStairs_recursion(self, n):
         """
         :type n: int
         :rtype: int
         """
-        if n == 1 or n == 2:
+        if n == 0:
+            return 0
+        elif n == 1:
             return 1
+        elif n == 2:
+            return 2
         else:
-            return self.climbStairs(n-1) + self.climbStairs(n-2)
+            return self.climbStairs_recursion(n - 1) + self.climbStairs_recursion(n - 2)
+
+    def slide_window_by_one(self, list_of_values):
+        value_to_be_appended = list_of_values[-1] + list_of_values[-2]
+        list_of_values.append(value_to_be_appended)
+        del(list_of_values[0])
+
+    def non_recurse(self, n, m):
+        total_ways = 0
+        if n == 0:
+            total_ways = 0
+        elif n == 1:
+            total_ways = 1
+        elif n == 2:
+            total_ways = 2
+        else:
+            last_m_elements = [0] * m
+            for index in range(0, n):
+                if index == 0:
+                    last_m_elements[index]=1
+                elif index ==1:
+                    last_m_elements[index]=1
+                else:
+                    self.slide_window_by_one(last_m_elements)
+            for index in range(0, m):
+                total_ways += last_m_elements[index]
+        return total_ways
+
 
 sol = Solution()
-print(sol.climbStairs2(400))
+total_stairs = 5
+for index in range(total_stairs):
+    print("Ways to climb {} stairs = {}.".format(index,
+                                                 sol.climbStairs_recursion(index)))
+ways = 3
+for index in range(total_stairs):
+    print("Ways to climb {} stairs max of {} stairs at a time  "
+          "using memo = {}.".format(index,
+                                    ways,
+                                    sol.climbStairs_recursion_n_steps_memo(
+                                        index,
+                                        ways)))
+
+
+ways = 3
+for index in range(total_stairs):
+    print("Non recursive: Ways to climb {} stairs max of {} stairs at a time  "
+          "using memo = {}.".format(index,
+                                    ways,
+                                    sol.non_recurse(
+                                        index,
+                                        ways)))
+
+"""
+1111
+112
+121
+22
+211
+"""
--- dynamic_programming/robber.py
+++ dynamic_programming/robber.py
@@ -16,9 +16,38 @@ Input: [2,7,9,3,1]
 Output: 12
 Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
              Total amount you can rob = 2 + 9 + 1 = 12.
-
-
+n = Total number of homes.
+P[] = value of each home.
+Max Revenue at nth home is
+a. If nth home is robbed.
+(nth home is included and max possible revenue at n-2 is computed.)
+b. If nth home is not robbed.
+(max revenue at n-1 is just returned)
+R(n) = max (P[n] + R(n-2), R(n-1))
+Base case: R(0) = 0, R(1) = 1
 """
+
+def find_max_profit2(current_index, profit_array, revenue_cache):
+    max_profit = 0
+    if current_index == 0:
+        max_profit = 1
+    elif current_index == 1:
+        max_profit = max(profit_array[0], profit_array[1])
+    else:
+        revenue_n_1 = 0
+        if not revenue_cache[current_index-1]:
+            revenue_n_1 = find_max_profit2(current_index-1,
+                                           profit_array,
+                                           revenue_cache)
+        revenue_n_2 = 0
+        if not revenue_cache[current_index-2]:
+            revenue_n_2 = find_max_profit2(current_index-2,
+                                           profit_array,
+                                           revenue_cache)
+        max_profit = max(profit_array[current_index] + revenue_n_2,
+                          revenue_n_1)
+    return max_profit
+
 def find_max_profit(nums):
     if not nums:
         return 0
@@ -29,10 +58,12 @@ def find_max_profit(nums):
         if i == 0:
             dp[i] = nums[i]
         elif i == 1:
-            dp[i] = max(nums[0],nums[1])
+            dp[i] = max(nums[0], nums[1])
         else:
             dp[i] = max(dp[i-1], dp[i-2]+nums[i])
     return dp[-1]
 
 nums = [2,7,1,3,9]
 print(find_max_profit(nums))
+cache = [0] * len(nums)
+print(find_max_profit2(len(nums)-1, nums, cache))

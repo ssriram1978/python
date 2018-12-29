97d7abf360bebac0154f38946d444f402149f3da
ssriram78 <sriramsridharjob8@gmail.com>
2018-12-29 20:50:06 +0000

    LCS and LCsubsequence.

--- dynamic_programming/longest_common_subsequence.py
+++ dynamic_programming/longest_common_subsequence.py
@@ -8,25 +8,55 @@ Example:
 """
 
 
-def lcs_recurse(X,Y,m,n,dp):
+def longest_common_subsequence_recurse(X, Y, m, n, dp, dp_string):
     if m == 0 or n == 0:
         return 0
-    if dp[m-1][n-1]:
-        return dp[m-1][n-1]
 
     if X[m-1] == Y[n-1]:
-        dp[m - 1][n - 1] = 1 + lcs_recurse(X,Y,m-1,n-1,dp)
-        return dp[m - 1][n - 1]
+        if dp[m-1][n-1] == -1:
+            dp[m-1][n-1] = longest_common_subsequence_recurse(X,
+                                                              Y,
+                                                              m - 1,
+                                                              n - 1,
+                                                              dp,
+                                                              dp_string)
+            dp_string.append(X[m - 1])
+        return 1 + dp[m-1][n-1]
     else:
-        dp[m - 1][n - 1] = max(lcs_recurse(X,Y,m,n-1,dp),
-                               lcs_recurse(X, Y, m - 1, n, dp))
-        return dp[m - 1][n - 1]
+        if dp[m][n-1] == -1:
+            dp[m][n-1] = longest_common_subsequence_recurse(X,
+                                                            Y,
+                                                            m,
+                                                            n - 1,
+                                                            dp,
+                                                            dp_string)
+        if dp[m-1][n] == -1:
+            dp[m-1][n] = longest_common_subsequence_recurse(X,
+                                                            Y,
+                                                            m - 1,
+                                                            n,
+                                                            dp,
+                                                            dp_string)
+        return max(dp[m][n-1], dp[m-1][n])
 
 
-def lcs(X,Y,m,n):
-    dp=[[0 for x in range(m)] for y in range(10000)]
-    return lcs_recurse(X,Y,m,n,dp)
+
+def longest_common_subsequence(X, Y, m, n, sub_sequence):
+    x = 0
+    if m > n:
+        x = m
+    else:
+        x = n
+
+    dp = [[-1 for x in range(x+1)] for y in range(x+1)]
+    return longest_common_subsequence_recurse(X, Y, m, n, dp, sub_sequence)
 
 X="ABAC BDAB"
 Y="BDCAB A"
-print("longest common subsequence length={}".format(lcs(X,Y,len(X),len(Y))))
+sub_sequence = []
+print("longest common subsequence length={}".format(longest_common_subsequence(X,
+                                                                               Y,
+                                                                               len(X),
+                                                                               len(Y),
+                                                                               sub_sequence)))
+print("dp_string={}.".format(sub_sequence))
